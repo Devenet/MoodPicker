0d8436318f03f32de2244343a5320e13889546ac
nicolabricot <github@devenet.info>
2014-09-26 23:19:04 +0200

    Update version to 1.1.0

--- library/picker/api.php
+++ library/picker/api.php
@@ -25,7 +25,7 @@ use Core\Config;
 
 class API extends \Core\API {
     
-    const VERSION = '1.0.0';
+    const VERSION = '1.1.0';
     const TOKENS_FILE = 'api_tokens';
 
     const API_PATH = "api/v1";
