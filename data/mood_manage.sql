8010370d9d99ee13729ed53721d71cf96ff5a13c
Nicolas Devenet <github@devenet.info>
2014-11-27 00:22:51 +0100

    Add setting class

--- data/mood_manage.sql
+++ data/mood_manage.sql
@@ -2,7 +2,7 @@ CREATE TABLE `users` (
     `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     `email` TEXT NOT NULL UNIQUE,
     `password` TEXT NOT NULL,
-    `last_login` INTEGER NOT NULL,
+    `last_login` INTEGER DEFAULT NULL,
     `last_ip` INTEGER DEFAULT '0.0.0.0',
     `privilege` INTEGER DEFAULT '0'
 );
---
+++ library/manage/setting.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+Copyright 2014 - Nicolas Devenet <nicolas@devenet.info>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Code source hosted on https://github.com/nicolabricot/MoodPicker
+*/
+
+namespace Manage;
+
+use Database\SQLite;
+
+class Setting {
+    
+    const DB = 'mood_manage';
+    private $db;
+
+    protected $id;
+    protected $name;
+    protected $value;
+
+    public function __construct($name = NULL) {
+        $this->db = SQLite::Instance(self::DB);
+        $this->id = NULL;
+
+        if (!empty($name)) {
+            $this->name = $name;
+
+            $q = $this->db->prepare('SELECT id, value FROM settings WHERE name = :name');
+            $q->execute(array( 'name' => $name ));
+            $data = $q->fetch();
+            $q->closeCursor();
+
+            if (!empty($data['id'])) {
+                $this->id = $data['id'];
+                $this->value = $data['value'];
+            }
+        }
+    }
+
+    public function exists() {
+        return !empty($this->id);
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function getValue() {
+        return $this->value;
+    }
+
+    public function setValue($value) {
+        $this->value = $value;
+    }
+
+    public function save() {
+        if ($this->exists()) {
+            $q = $this->db->prepare('UPDATE settings SET value = :value WHERE id = :id');
+            $row_updated = $q->execute(array(
+                'value' => $this->value,
+                'id' => $this->id
+            ));
+            $q->closeCursor();
+            return $row_updated == 1;
+        }
+        if (!empty($this->name) && !empty($this->value)) {
+            $q = $this->db->prepare('INSERT INTO settings(name, value) VALUES(:name, :value)');
+            $q->execute(array(
+                'value' => $this->value,
+                'name' => $this->name
+            ));
+            $q->closeCursor();
+
+            $q = $this->db->query('SELECT last_insert_rowid() AS last_row FROM api_request');
+            $data = $q->fetch();
+            $q->closeCursor();
+
+            $this->id = isset($data['last_row']) ? $data['last_row'] : NULL;
+
+            return $this->exists();
+        }
+        return false;
+    }
+
+    public function delete() {
+        if ($this->exists) {
+            $q = $this->db->prepare('DELETE FROM settings WHERE id = :id');
+            $row_updated = $q->execute(array( 'id' => $this->id ));
+            $q->closeCursor();
+            return $row_updated == 1;
+        }
+    }
+
+}
+
+?>
