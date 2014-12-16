e516046165291678c52c1a9844f085805c09ab36
Nicolas Devenet <github@devenet.info>
2014-12-16 23:27:42 +0100

    Improve and fix css, js & templates

--- assets/js/moodpicker.js
+++ assets/js/moodpicker.js
@@ -3,4 +3,8 @@ $(function() {
     $('.navigate a, a.navigate').on('click', function() { $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top}); });
     $('a[rel="external"]').click(function() { window.open($(this).attr('href')); return false; });
 	$('.tip').tooltip();
+	var alert_message = $('.alert.message .close');
+	function closeMessage() { alert_message.parent().slideUp(500, function(){ location.href = location.pathname; }); }
+	if(alert_message.length > 0) { setTimeout(closeMessage, 5000); }
+	alert_message.on('click', closeMessage);
 });
